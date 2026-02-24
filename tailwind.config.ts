53c6f7fc957887300c2ba3401448066b4665e10f
Twenty20mathsclass <twenty20mathsclass@gmail.com>
2026-02-24 17:01:27 +0000

    Make my fonts as inter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"Bricolage Grotesque"', 'Inter', 'sans-serif'],
-        headline: ['"Bricolage Grotesque"', 'Inter', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
