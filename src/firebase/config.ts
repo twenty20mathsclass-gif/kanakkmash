4572446a4a372252aaf3c25a48caf97029a7da28
Twenty20mathsclass <twenty20mathsclass@gmail.com>
2026-02-24 09:27:04 +0000

    This is the sdk file for env
    // Import the functions you need from the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,5 +19,10 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+
+if (typeof window !== 'undefined') {
+  // Initialize Analytics only on the client side
+  getAnalytics(app);
+}
 
 export { app, firestore, auth, storage, firebaseConfig };
