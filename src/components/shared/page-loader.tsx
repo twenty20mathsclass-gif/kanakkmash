cdefb96d9644934135eaf872591b2a0e4b61fa86
MUHAMMED YASEEN VATTAPPARA <mhdyaseenvattappara@gmail.com>
2026-03-09 15:27:25 +0000

    make loading small in mobile view

--- src/components/shared/page-loader.tsx
+++ src/components/shared/page-loader.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 export function PageLoader({ fullScreen = true }: { fullScreen?: boolean }) {
   const loaderContent = (
-    <div className="grid h-64 w-64 place-content-center">
+    <div className="grid h-48 w-48 place-content-center md:h-64 md:w-64">
       {/* The spinner div, absolutely positioned */}
       <div
-        className="col-start-1 row-start-1 h-56 w-56 animate-spin rounded-full border-8 border-solid border-transparent border-t-primary border-r-accent"
+        className="col-start-1 row-start-1 h-40 w-40 animate-spin rounded-full border-8 border-solid border-transparent border-t-primary border-r-accent md:h-56 md:w-56"
         role="status"
       >
         <span className="sr-only">Loading...</span>
@@ -20,7 +20,7 @@ export function PageLoader({ fullScreen = true }: { fullScreen?: boolean }) {
           alt="Loading..."
           width={128}
           height={128}
-          className="h-32 w-32"
+          className="h-24 w-24 md:h-32 md:w-32"
           priority
           unoptimized
         />
