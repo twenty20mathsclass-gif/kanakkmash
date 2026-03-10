0e1bb993a7155d5097c3bb53d0af8f83cfb90375
Twenty20mathsclass <twenty20mathsclass@gmail.com>
2026-03-10 06:23:45 +0000

    But still there is the gap

--- src/components/shared/main-layout-client.tsx
+++ src/components/shared/main-layout-client.tsx
@@ -210,7 +210,7 @@ export default function MainLayoutClient({
           </>
         )}
       </Suspense>
-      <main className="flex-grow p-4 pt-28 pb-28 md:pt-24 md:px-6 lg:px-8">{children}</main>
+      <main className="flex-grow p-4 pt-20 pb-28 md:pt-24 md:px-6 lg:px-8">{children}</main>
       {isPubliclyAccessible && (
         <footer className="bg-background py-6">
           <div className="container mx-auto flex items-center justify-center px-4 md:px-6">
